45a4c1add6b075e30f4c33e372c5c36833e46bd3
unknown <N.tchorowski@gmail.com>
2013-04-17 21:32:36 -0400

    Working chat on the client side, need to update for sever side.

--- MyFirstApp/gen/com/example/myfirstapp/R.java
+++ MyFirstApp/gen/com/example/myfirstapp/R.java
@@ -24,14 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080005;
-        public static final int button2=0x7f080004;
-        public static final int clientChat=0x7f080002;
-        public static final int digitalClock1=0x7f080001;
-        public static final int editText1=0x7f080000;
-        public static final int edit_message=0x7f080003;
-        public static final int serverChat=0x7f080006;
+        public static final int SCROLLER_ID=0x7f080001;
+        public static final int action_settings=0x7f08000b;
+        public static final int button1=0x7f080008;
+        public static final int button2=0x7f080007;
+        public static final int clientChat=0x7f080003;
+        public static final int client_chat_area=0x7f080004;
+        public static final int digitalClock1=0x7f080000;
+        public static final int editText1=0x7f080002;
+        public static final int editText2=0x7f08000a;
+        public static final int edit_message=0x7f080006;
+        public static final int sendButton=0x7f080005;
+        public static final int serverChat=0x7f080009;
     }
     public static final class layout {
         public static final int activity_client=0x7f030000;
@@ -55,10 +59,11 @@ public final class R {
         public static final int find_ip=0x7f050003;
         public static final int hello_world=0x7f05000c;
         public static final int menu_settings=0x7f050007;
-        public static final int server_phone=0x7f05000e;
+        public static final int send_button=0x7f05000e;
+        public static final int server_phone=0x7f05000f;
         public static final int set_as_client=0x7f050005;
         public static final int set_as_server=0x7f050004;
-        public static final int title_activity_client=0x7f05000f;
+        public static final int title_activity_client=0x7f050010;
         public static final int title_activity_display_message=0x7f05000a;
         public static final int title_activity_main=0x7f050008;
         public static final int title_activity_server=0x7f05000b;
--- MyFirstApp/src/com/example/myfirstapp/ClientActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/ClientActivity.java
@@ -16,46 +16,53 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ClientActivity extends Activity {
-	//For the interface section
-	//Change this editText and button to the UI shit section
-	
+	// For the interface section
+	// Change this editText and button to the UI shit section
+
 	String sendStr = null;
 	String serverOut;
-	String server; //server is user input - will be default of "10.0.2.2"
-	    
+	String server; // server is user input - will be default of "10.0.2.2"
+
 	private EditText serverIP;
 	private Button connect;
-	
+
 	private String serverIPAddr = "NOT AN IP ADDRESS";
-	private boolean connectionEstablished  = false;
+	private String userMessage;
+	private boolean connectionEstablished = false;
 	private Handler handler = new Handler();
 	private boolean connectonEstablished;
 	private int activity_server;
-		
-	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_client);		
+		setContentView(R.layout.activity_client);
 		server = "10.0.2.2";
-		
-		Intent intent = getIntent();
-		String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-		
-		EditText editText = (EditText) findViewById(R.id.clientChat);
-	   	editText.append(message);
-		
 
+		Intent intent = getIntent();
 		System.out.println("UN>PBUC>RDUR");
 		// TODO Auto-generated catch block
 		serverIP = (EditText) findViewById(activity_server);
 		Thread client = new Thread(new ClientThread());
 		client.run();
+		
+
+		String message = serverOut; // intent.getStringExtra(DisplayMessageActivity.EXTRA_MESSAGE);
+
+		TextView editText = (TextView) findViewById(R.id.clientChat);
+		editText.append(message);
 	}
 
+	public void forwardMessage(View view) {
+		EditText userArea = (EditText) findViewById(R.id.client_chat_area);
+		userMessage = "You: " + userArea.getText().toString() + "\n";
+		
+		TextView editText = (TextView) findViewById(R.id.clientChat);		
+		editText.append(userMessage);
+	}
 
 	public class ClientThread implements Runnable {
 		@Override
@@ -64,14 +71,15 @@ public class ClientActivity extends Activity {
 			try {
 				clientSocket = new Socket(server, 8080);
 				connectonEstablished = true;
-				
-				DataOutputStream outToServer =
-						new DataOutputStream(clientSocket.getOutputStream());
-						
+
+				DataOutputStream outToServer = new DataOutputStream(
+						clientSocket.getOutputStream());
+
 				outToServer.writeBytes("Sent message \n");
-			    BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			    
-		        serverOut = inFromServer.readLine();
+				BufferedReader inFromServer = new BufferedReader(
+						new InputStreamReader(clientSocket.getInputStream()));
+
+				serverOut = "Server:" + inFromServer.readLine() + "\n";
 			} catch (UnknownHostException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -79,12 +87,10 @@ public class ClientActivity extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-				
-			
+
 		}
 	}
 
- 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
